91a354fefcf495187a248a339f4d4b9d3b496945
Hein <heindauven@gmail.com>
2022-04-01 09:07:52 +0200

    Add 1 million tokens mint on deploy.

--- contracts/CBToken.sol
+++ contracts/CBToken.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CBToken is ERC20 {
     constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {
-        
+        _mint(msg.sender, 1_000_000 * (10**decimals()));
     }
 }
