4f92ba5d34aa11521f1ab4621d4a043e1aed628c
Hein <heindauven@gmail.com>
2022-04-01 09:43:18 +0200

    Add onlyOwner modifier.

--- contracts/CBToken.sol
+++ contracts/CBToken.sol
@@ -4,7 +4,16 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CBToken is ERC20 {
-    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {
+    address public owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner");
+        _;
+    }
+
+    constructor(string memory name_, string memory symbol_)
+        ERC20(name_, symbol_)
+    {
         _mint(msg.sender, 1_000_000 * (10**decimals()));
     }
 }
